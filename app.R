a87a5125cc60ac628a386884a0f15ea10a7083d2
Mona Thiele <mona.christin.thiele@gmail.com>
2023-04-18 15:36:30 +0200

    current updates

--- app.R
+++ app.R
@@ -10,6 +10,7 @@
 # UPDATE CURRENT R VERSION 
 install.packages("installr")
 install.packages("devtools")
+install.packages("plotly")
 devtools::install_github("andreacirilloac/updateR")
 library(updateR)
 updateR()
@@ -18,57 +19,75 @@ R.Version()$version.string
 
 
 # LOAD Packages
-install.packages(c("shiny", "readxl", "dplyr"))
+install.packages(c("shiny", "readxl", "dplyr", "tidyverse", "lubridate"))
 library(shiny)
 library(ggplot2)
 library(readxl)
 library(dplyr)
+library(plotly)
+library(RColorBrewer)
+library(tidyverse)
+library(lubridate)
 
-#read data (from Kaggel: https://www.kaggle.com/datasets/dc0e09e5c6a808eedf06d57474263925202d84f9c35fabd6a447eab52fb1957f?resource=download )
+# read data (from Kaggle: https://www.kaggle.com/datasets/dc0e09e5c6a808eedf06d57474263925202d84f9c35fabd6a447eab52fb1957f?resource=download )
 layoffs <- read.csv("layoffs.csv")
 head(layoffs)
 
 # Convert total_laid_off and percentage_laid_off to numeric
 layoffs$total_laid_off <- as.numeric(layoffs$total_laid_off)
 layoffs$percentage_laid_off <- as.numeric(layoffs$percentage_laid_off)
+layoffs$date <- as.Date(layoffs$date , format = "%m/%d/%Y")
 
 
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
-  titlePanel("Layoffs by Industry"),
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "industry",
-                  label = "Choose an Industry:",
-                  choices = sort(unique(layoffs$industry)),
-                  selected = NULL)
+      selectInput("industry", "Select an Industry:",
+                  choices = c("All", unique(layoffs$industry)))
     ),
     mainPanel(
-      h3("Total Laid Off:"),
-      verbatimTextOutput("total_laid_off"),
-      h3("Percentage Laid Off:"),
-      verbatimTextOutput("percentage_laid_off")
+      plotOutput("industry_plot", height = "300px"),
+      plotOutput("time_plot", height = "300px")
     )
   )
 )
 
-
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  industry_data <- reactive({
-    filter(layoffs, industry == input$industry)
+  # Filter data by selected industry
+  filtered_layoffs <- reactive({
+    if (input$industry == "All") {
+      return(layoffs)
+    } else {
+      return(layoffs %>% filter(industry == input$industry))
+    }
   })
   
-  output$total_laid_off <- renderPrint({
-    sum(industry_data()$total_laid_off, na.rm = TRUE)
+  # Bar plot of percentages laid off by industry
+  output$industry_plot <- renderPlot({
+    ggplot(filtered_layoffs(), aes(x = industry, y = percentage_laid_off, fill = industry)) +
+      geom_bar(stat = "identity") +
+      labs(x = "Industry", y = "Percentage Laid Off", title = "Percentage Laid Off by Industry") +
+      theme_minimal()
   })
   
-  output$percentage_laid_off <- renderPrint({
-    paste0(round(sum(industry_data()$percentage_laid_off, na.rm = TRUE), 2), "%")
+  # Line plot of total layoffs over time
+  output$time_plot <- renderPlot({
+    filtered_data <- filtered_layoffs() %>% 
+      filter(!is.na(date)) %>% # Remove rows with missing dates
+      mutate(date = as.Date(date, format = "%m/%d/%Y")) # Convert date to proper format
+    ggplot(filtered_data, aes(x = date, y = total_laid_off)) +
+      geom_line() +
+      labs(x = "Date", y = "Total Laid Off", title = "Total Layoffs over Time") +
+      theme_minimal()
   })
+  
   
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
+
+
+anyNA(layoffs$date)
+which(is.na(layoffs$date))
